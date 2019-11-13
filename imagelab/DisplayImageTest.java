906564234189dc1564dc1c220ff1c7e50834479e
[Ryan Peterson] <[alpharius201@gmail.com]>
2019-11-12 19:38:37 -0700

    Committing current state of the DisplayImageTest.java and some build changes to include junit functionality.

--- imagelab/DisplayImageTest.java
+++ imagelab/DisplayImageTest.java
@@ -1,15 +1,22 @@
 package imagelab;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.*;
 
 /** Unit test for DisplayImage.java
+ * @author Ryan Peterson
  */
 
 public class DisplayImageTest {
 
+    String testString = "TEST";
+
+    /**
+     * Test for constructor
+     */
     @Test
     public void constructorTest () {
-        String testString = "TEST";
         DisplayImage DisplayTest = new DisplayImage(null, testString);
     }
 
