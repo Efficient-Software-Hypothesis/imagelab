103d1278432c1190b4fff66723a44898f6a2beb0
[Ryan Peterson] <[alpharius201@gmail.com]>
2019-11-14 15:21:40 -0700

    added TODO to displayImageTest.java

--- imagelab/DisplayImageTest.java
+++ imagelab/DisplayImageTest.java
@@ -17,7 +17,22 @@ public class DisplayImageTest {
      */
     @Test
     public void constructorTest () {
+        /**
+         * TODO iterate through the images directory to ensure they all construct as a proper
+         * constructor test. Each iterator constructs the class and passes a different imp in
+         * the directory. Then deconstructs the class before iterating to the next imp in the
+         * directory.
+         */
         DisplayImage DisplayTest = new DisplayImage(null, testString);
     }
+
+    /**
+     * TODO write test for imp NULL situation to esnure that the correct .println
+     * output is given, as per the source code
+     */
+
+    /**
+     * TODO write event tracking test.
+     */
 
 }
